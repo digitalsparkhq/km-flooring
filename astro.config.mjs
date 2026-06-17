24aac8bd70a3b02310f440122cf3c804577b83a5
Digital Spark HQ <info@digitalsparkhq.com>
2026-06-17 07:25:41 +0530

    fix: updated the content

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kmfloors.co.uk',
+  site: 'https://krmflooringltd.co.uk',
 
   vite: {
     plugins: [tailwindcss()]
